d93221c533028ee1c35fabf87cc61f58e9c4a36f
hemant modi <142676792+Hemant-1993@users.noreply.github.com>
2023-12-03 12:21:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function days_of_a_year(year) 
+{
+   
+  return isLeapYear(year) ? 366 : 365;
+}
+
+function isLeapYear(year) {
+     return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+}
